26418e4802c8f30a71bedb3589720fd4bf216681
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-08-22 19:57:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='h-screen w-screen flex justify-center items-center font-bold text-4xl tracking-tight	'>
+        This is the basic app
+      </h1>
+        
+    </>
+  )
+}
+
+export default App
